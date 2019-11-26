43c528867243789f140e28b87b1b95f085bcaac7
migmartinez <migmart@umich.edu>
2019-11-26 07:50:39 -0500

    Update 206W17_HW3.py

--- 206W17_HW3.py
+++ 206W17_HW3.py
@@ -3,23 +3,13 @@ import re
 
 ## SI 206 - W17 - HW3
 ## Miguel Martinez
-## COMMENT WITH:
-## Your section day/time: Thursday, 6-7pm.
-## Any names of people you worked with on this assignment: N/A.
+
 
 #####################
 
 
-## PART 1: 300 points
 
-## Use regular expressions to define a function called parse_counted_words which should accept a string, find strings of the form: <count> <word>  e.g. 101 Dalmations, inside it, and should return either the LAST correctly matching number-word combo in the string as a tuple, e.g. ('13', "pineapples"), if there are any such sub-strings, or None, if there are not.
-## The number in the <count> should be one or more digits only. The word should be made up of an optional non-alphabetic character followed by any number of alphabetic characters, upper or lower case.
-## HINT:  \b matches the beginning or end of a word
-## HINT:  you can use the Python re .findall method to get multiple matches in a string
-#parse_counted_words('5 watermelons, 13 pineapples, and 1 papaya.') should return ('1', 'papaya')
-# parse_counted_words('101 dalmations!') should return ('101', 'dalmations') ...
-
-## Write code to define your parse_counted_words function here.
+##Regex Exercise
 
 def parse_counted_words(string):
     pattern1 = "\\b(\d+)\\s(.[A-Za-z]+)"
@@ -28,11 +18,7 @@ def parse_counted_words(string):
         return answer[-1]
     return None
 
-## PART 2: 200 points
 
-## We have provided a text file computer_paths.txt. It's not incredibly long -- you can scan through it, but do NOT hard code your answers! Each line contains 1 filesystem path.
-
-## (a) Write Python code to determine how many of these paths identify FILES, not directories. Save that number in the variable file_paths_num.
 infile2a = open("./computer_paths.txt", "r")
 pattern2a = r"\d*[.]"
 file_lst = []
@@ -42,7 +28,7 @@ for aline in infile2a:
         file_lst.append(aline)
 file_paths_num = len(file_lst)
 infile2a.close()
-## (b) Write Python code to determine how many of these paths are FULL paths, not relative paths. Save that number in the variable full_paths_num.
+
 infile2b = open('./computer_paths.txt', 'r')
 pattern2b = r'^[/~]'
 full_lst = []
@@ -52,7 +38,7 @@ for bline in infile2b:
         full_lst.append(bline)
 full_paths_num = len(full_lst)
 infile2b.close()
-## (c) Write Python code to determine how many of these paths describe a Python file saved inside a folder called SI206. Save that number in the variable python_course_paths.
+
 infile2c = open('./computer_paths.txt', 'r')
 pattern2c = r'[SI][206]\S+[.][py]'
 py_lst = []
@@ -62,7 +48,7 @@ for cline in infile2c:
         py_lst.append(cline)
 python_course_paths = len(py_lst)
 infile2c.close()
-## (d) Write Python code to determine how many of these paths describe a Microsoft file (a file that EITHER ends with .docx OR .xlsx, but nothing else counts) where the file name ends in a digit. Save that total in the variable microsoft_files_num.
+
 infile2d = open('./computer_paths.txt', 'r')
 pattern2d = r'\d[.][xlsd]'
 micro_lst = []
@@ -72,10 +58,9 @@ for dline in infile2d:
         micro_lst.append(dline)
 microsoft_files_num = len(micro_lst)        
 infile2d.close()
-## We have provided unit tests in this file. To earn the full 500 points, you'll need to pass all of the tests and will need to have followed the instructions.
-## Each class of the tests represents one "part" of the homework, and the points for each part are divided approx. equally between each of the tests.
 
-####### UNIT TESTING BELOW; DO NOT CHANGE ANY TESTING CODE #######
+
+####### UNIT TESTING BELOW #######
 
 class Part1_HW3(unittest.TestCase):
     def test_pcw_1(self):
