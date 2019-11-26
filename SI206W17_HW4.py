b346ee50c8119532adc7a6f6d35de41d1dff5901
migmartinez <migmart@umich.edu>
2019-11-26 07:48:10 -0500

    Update SI206W17_HW4.py

--- SI206W17_HW4.py
+++ SI206W17_HW4.py
@@ -5,14 +5,11 @@ from bs4 import BeautifulSoup
 
 ## SI 206 - W17 - HW4
 ## Miguel Martinez
-## COMMENT WITH:
-## Your section day/time: Thursdays, 6-7pm
-## Any names of people you worked with on this assignment: N/A.
+
 
 #####################
 
-## PART 1 (100 points) - Get the HTML data from http://www.nytimes.com (the New York Times home page) and save it in a file called nytimes_data.html.
-## Write the Python code to do so here.
+
 #get HTML data
 html_data = requests.get('http://www.nytimes.com').text
 infile = open('nytimes_data.html', 'w', encoding='utf-8')
@@ -20,29 +17,7 @@ infile.write(html_data)
 infile.close()
 #####################
 
-## PART 2 (200 points)
-## Write code to get the first 10 headlines from the New York Times, based on the data you saved in the file in Part 1, and save those strings in a list called nytimes_headlines.
 
-## Note that you will almost certainly need to do some investigation on the http://nytimes.com website to do this correctly, even after saving the file in Part 1.
-
-## The strings that should be elements of your lists will be different depending upon when you accessed the data, but they should probably be somewhat like this:
-
-## U.S. Files Charges Against Rahami in Series of Bombings
-## In a Play, a Take of a Bombing. A Block Away, a Real Explosion
-## The Interpreter: The War We'd Rather Not Talk About
-
-## .. et cetera, for 10 headline strings.
-
-## Weed out the headlines that do not have any text, for instance the headlines that are linked to pictures or video but without any text content. You should not add these to the list nor count them towards the limit of 10. Ignore them completely.
-
-## Not sure where to start? This link might very helpful: http://www.practicepython.org/solution/2014/07/10/17-decode-a-web-page-solutions.html
-
-## We have included a couple of unit tests to check some things about your result, but we will also be checking your code against our OWN version of a nytimes_data.html file, which will almost certainly be different from yours, and your code should still work to get the first 10 text headlines from our file!
-
-## Write your code to complete this task here.
-## HINT: Remember that you'll need to open the file you created in Part 1, read the contets into one big string, and make a BeautifulSoup object out of that string!
-## NOTE that the provided link does not include saving the online data in a file as part of the process. But it still provides very useful hints/tricks about how to look for and identify the headlines on the NY Times page.
-# code to get first 10 headline strings
 infile2 = open('nytimes_data.html', 'r', encoding='utf-8')
 soup = BeautifulSoup(infile2, 'html.parser')
 nytimes_long_headlines = []
@@ -53,20 +28,7 @@ nytimes_headlines = nytimes_long_headlines[0:10]
 
 #####################
 
-## PART 3 (200 points)
 
-## Go to this URL: https://www.si.umich.edu/directory?field_person_firstname_value=&field_person_lastname_value=&rid=All
-
-## Use the requests library (or other Python code that has the same effect) and Beautiful Soup to gather, from that web page, each of the names and the titles.
-## You should create a dictionary called umsi_titles which contains the names and the titles for each person on that page. For example, your dictionary should include the following key-value pairs (and a bunch more...):
-
-## "Lindsay Blackwell":"PhD Student"
-## "Reginald Beasley":"Admissions and Student Affairs Assistant"
-## "Daniel Atkins III":"Professor Emeritus of Information, School of Information and Professor Emeritus of Electrical Engineering and Computer Science, College of Engineering"
-
-## We have provided unit tests for this problem which you must pass to gain all 200 points for this part.
-
-## Write code to complete this task here. We've gotten you started... note that it'll be difficult to continue if you don't understand what the provided code does!
 
 response = requests.get("https://www.si.umich.edu/directory?field_person_firstname_value=&field_person_lastname_value=&rid=All")
 htmldoc = response.text
@@ -75,13 +37,7 @@ soup = BeautifulSoup(htmldoc,"html.parser")
 people = soup.find_all("div",{"class":"views-row"})
 umsi_titles = {}
 #print(people)
-## It may be helpful to translate the following from English to code:
 
-## For each element in the list saved in the variable people,
-## Find the container that holds the name that belongs to that person (HINT: look for something unique, like a property element...)
-## Find the container that holds the title that belongs to that person (HINT: a class name)
-## Grab the text of each of those elements and put them in the dictionary umsi_titles properly
-# cache data that contains names and titles for each person on page, save in dictionary.
 response = requests.get("https://www.si.umich.edu/directory?field_person_firstname_value=&field_person_lastname_value=&rid=All")
 htmldoc = response.text
 
@@ -102,8 +58,8 @@ print(umsi_titles)
 
 
 
-######### UNIT TESTS; DO NOT CHANGE ANY CODE BELOW THIS LINE #########
-#### NOTE: hard-coding to pass any of these tests w/o following assignment instructions is not acceptable for points
+######### UNIT TESTS ######### 
+
 
 class HW4_Part2(unittest.TestCase):
 	def test_first_last_elem(self):
